288c2b3b0cea99a79b21292ea11fa1889433dab4
Elrohil44 <w.stanek123@gmail.com>
2017-03-15 11:30:57 +0100

    Deleted rubbish

--- 1/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/student/Desktop/SysOpy/1
-# Build directory: /home/student/Desktop/SysOpy/1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(dynamic "./main_dynamic")
-add_test(shared "./main_shared")
-add_test(static "./main_static")
