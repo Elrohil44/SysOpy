7629d6965332e8a56aa803287c6579fc60c53e39
Elrohil44 <w.stanek123@gmail.com>
2017-04-04 12:52:07 +0200

    4.2 fix

--- 4/zad2/child.c
+++ 4/zad2/child.c
@@ -13,7 +13,7 @@ clock_t start_time;
 clock_t last_time;
 static int loop = 1;
 union sigval bbb;
-
+pid_t parent;
 
 void printtime(double time)
 {
@@ -24,14 +24,14 @@ void child_handler(int sig){}
 
 int main(int argc, char const *argv[]) {
   srand(time(NULL));
-
+  parent = getppid();
   struct sigaction action;
   action.sa_handler = child_handler;
   sigemptyset(&action.sa_mask);
   action.sa_flags = 0;
   sigaction(SIGUSR2, &action, NULL);
 
-  sleep(1000 * rand()%11);
+  sleep(rand()%11);
   bbb.sival_int = rand();
   sigset_t set;
   siginfo_t info;
@@ -51,29 +51,37 @@ int main(int argc, char const *argv[]) {
 
 
   clock_gettime(CLOCK_MONOTONIC, &start);
-  if(sigqueue(getppid(), SIGUSR1, bbb))
+  printf("Sending signal SIGUSR1 from %d to %d\n", getpid(), parent);
+  fflush(stdout);
+  if(sigqueue(parent, SIGUSR1, bbb))
   {
-    printf("Couldn't send signal SIGUSR1 from %d to %d\n", getpid(), getppid());
+    printf("Couldn't send signal SIGUSR1 from %d to %d\n", getpid(), parent);
   }
   int a;
   while (loop) {
     a = sigtimedwait(&set, &info, &timeout);
-    if(a == SIGINT) {loop = 0;}
+    if(a == SIGINT) {exit(EXIT_FAILURE);}
     else if(a == SIGUSR2)
     {
-      if(sigqueue(getppid(), SIGRTMIN + rand()%32, bbb))
-        printf("Error while sending signal SIGRT from %d to %d\n",getpid(), getppid());
+	printf("Child %d: Received signal SIGUSR2 from %d\n",getpid(), info.si_pid);
+  fflush(stdout);
+      printf("Sending signal SIGRT from %d to %d\n",getpid(), parent);
+      fflush(stdout);
+      if(sigqueue(parent, SIGRTMIN + rand()%32, bbb))
+        printf("Error while sending signal SIGRT from %d to %d\n",getpid(), parent);
+        else loop = 0;
         clock_gettime(CLOCK_MONOTONIC, &finish);
         elapsed = (finish.tv_sec - start.tv_sec);
         elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
         printtime(elapsed);
-        loop = 0;
     }
     else if(a == -1 && errno == EAGAIN)
     {
-      if(sigqueue(getppid(), SIGUSR1, bbb))
+       printf("Sending signal SIGUSR1 from %d to %d\n", getpid(), parent);
+       fflush(stdout);
+      if(sigqueue(parent, SIGUSR1, bbb))
       {
-        printf("Couldn't send signal SIGUSR1 from %d to %d\n", getpid(), getppid());
+        printf("Couldn't send signal SIGUSR1 from %d to %d\n", getpid(), parent);
       }
     }
   }
--- 4/zad2/main.c
+++ 4/zad2/main.c
@@ -22,11 +22,11 @@ int isNumber(const char* arg)
 }
 
 
-static int requests = 0;
-static pid_t* pids;
-static int i = -1;
+sig_atomic_t requests = 0;
+static sig_atomic_t* pids;
+sig_atomic_t i = -1;
 static int M;
-static int N, _N;
+static int volatile N, _N;
 static int loop = 1, loop1 = 1;
 
 
@@ -42,6 +42,7 @@ void g(int sig, siginfo_t* a, void* b)
   switch (sig) {
     case SIGUSR1:
     printf("Received signal SIGUSR1 from %d\n", a->si_pid);
+    fflush(stdout);
     if(++requests < M)
     {
       for(int l=i+1;l < N;l++)
@@ -54,7 +55,8 @@ void g(int sig, siginfo_t* a, void* b)
     else
     {
       loop1 = 0;
-
+      printf("Sending signal SIGUSR2 from %d to %d\n",getpid(), a->si_pid );
+      fflush(stdout);
       if(kill(a->si_pid, SIGUSR2) == -1)
         printf("Error while sending signal SIGUSR2 from %d to %d\n",getpid(), a->si_pid );
     }
@@ -63,9 +65,15 @@ void g(int sig, siginfo_t* a, void* b)
     printf("Received signal SIGINT\n");
     for(int i=0; i<_N; i++)
     {
+
       if(kill(pids[i], 0) == 0 && pids[i]!=-1)
+	{
+
+        printf("Sending signal SIGINT from %d to %d\n",getpid(), pids[i]);
+        fflush(stdout);
         if(kill(pids[i], SIGINT) == -1)
         printf("Error while sending signal SIGINT from %d to %d\n",getpid(), pids[i]);
+	}
     }
     exit(EXIT_FAILURE);
     break;
@@ -73,6 +81,7 @@ void g(int sig, siginfo_t* a, void* b)
     if(sig <= SIGRTMAX && sig >= SIGRTMIN)
     {
       printf("Received exit code %d from %d\n", sig, a->si_pid);
+      fflush(stdout);
       for(int i=0; i<_N; i++) if(pids[i]==a->si_pid) pids[i] = -1;
       N--;
     }
@@ -88,7 +97,8 @@ int main(int argc, char const *argv[]) {
   struct sigaction action;
   action.sa_sigaction = g;
   sigemptyset(&action.sa_mask);
-
+  //sigaddset(&action.sa_mask, SIGUSR1);
+  //for (int i= SIGRTMIN;i<=SIGRTMAX;i++) sigaddset(&action.sa_mask, i);
   action.sa_flags = SA_SIGINFO;
   sigaction(SIGUSR1, &action, NULL);
   sigaction(SIGINT, &action, NULL);
