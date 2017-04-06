736276e8398a50e22a76db2fcdd1545e4b0f5ce2
Elrohil44 <w.stanek123@gmail.com>
2017-04-06 02:28:50 +0200

    I think 5.1 is complete

--- 5/zad1/shell.c
+++ 5/zad1/shell.c
@@ -11,61 +11,103 @@
 
 void printexec(char* const* args);
 
-// void printusage(const char * name)
-// {
-//   printf("Usage:\t %s Batch_file:\n\n", name);
-//   printf("\tBatch_file - path to file containing a batch script\n");
-//   exit(0);
-// }
-
 int parseline(char* line, char** programs)
 {
   int i=0;
-  if((args[0]=strtok(line, "|\n"))!=NULL)
-    while (i<MAX_PROGRAMS-2 && (args[++i]=strtok(NULL, "|\n"))!=NULL);
+  if((programs[0]=strtok(line, "|\n"))!=NULL)
+    while (i<MAX_PROGRAMS-2 && (programs[++i]=strtok(NULL, "|\n"))!=NULL);
   programs[i]=NULL;
-  return 1;
+  return i;
 }
 
 int parseargs(char* program, char** args)
 {
   int i=0;
-  if((args[0]=strtok(line, " \t\r\n"))!=NULL)
+  if((args[0]=strtok(program, " \t\r\n"))!=NULL)
     while (i<MAX_ARGS-2 && (args[++i]=strtok(NULL, " \t\r\n"))!=NULL);
-  programs[i]=NULL;
-  return 1;
+  args[i]=NULL;
+  return i;
 }
 
-// void printexec(char* const* args)
-// {
-//   if(args==NULL) return;
-//   for(int i=0;args[i]!=NULL;i++) printf("%s ", args[i]);
-//   printf("\n");
-// }
+void printexec(char* const* args)
+{
+  if(args==NULL) return;
+  for(int i=0;args[i]!=NULL;i++) printf("%s ", args[i]);
+  printf("\n");
+}
+
+void swappipes(int** pipe1, int** pipe2)
+{
+  int* tmp = *pipe1;
+  *pipe1 = *pipe2;
+  *pipe2 = tmp;
+}
 
 int main(int argc, char const *argv[]) {
   char* args[MAX_ARGS];
   char* programs[MAX_PROGRAMS];
-  int pipes[MAX_PROGRAMS][2];
+  pid_t pids[MAX_PROGRAMS];
+  int pipeR[2];
+  int pipeW[2];
   char* line=NULL;
   size_t n=0;
-  pid_t pid,w;
-  int programsc, argsc;
+  int programsc;
   int status;
+  int f = 1;
+  printf(">>> ");
+  fflush(stdout);
   while((getline(&line,&n,stdin))!=-1)
   {
     programsc = parseline(line, programs);
     for(int i=0; i<programsc; i++)
     {
-      pipe(pipes[i]);
+      if(i>0 && f) swappipes((int**)&pipeW, (int**)&pipeR);
+      if(i==0) pipe(pipeW);
       if(parseargs(programs[i], args))
       {
-        
-      };
 
+          f = 1;
+          if((pids[i] = fork())==0)
+          {
+
+            if(i<programsc - 1)
+            {
+            close(pipeW[0]);
+            if(dup2(pipeW[1], STDOUT_FILENO)==-1) printf("WutwUt\n");
+            close(pipeW[1]);
+
+            // close(pipeW[0]);
+            }
+            if(i>0)
+            {
+              close(pipeR[1]);
+              if(dup2(pipeR[0],STDIN_FILENO) == -1) printf("WutWut\n");
+              close(pipeR[0]);
+
+            }
+            if(execvp(*args, args) == -1) exit(EXIT_FAILURE);
+          }
+      }
+      else
+      {
+        f = 0;
+        continue;
+      }
+      if(i>0)
+      {
+        close(pipeR[0]);
+        close(pipeR[1]);
+      }
+      if(i<programsc - 1) pipe(pipeR);
     }
-
-
+    close(pipeW[1]);
+    close(pipeW[0]);
+    // close(pipeR[0]);
+    // close(pipeR[1]);
+    for(int i=0 ; i<programsc; i++)
+    {
+      waitpid(pids[i], &status, 0);
+    }
     printf(">>> ");
     fflush(stdout);
   }
