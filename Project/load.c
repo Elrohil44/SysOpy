985ea9316c82af53b021b0fbbb8065ca8b804a7d
Elrohil44 <w.stanek123@gmail.com>
2017-06-06 23:04:45 +0200

    Bugfix for x86-64

--- Project/load.c
+++ Project/load.c
@@ -4,9 +4,16 @@
 #include <sys/time.h>
 #include <sys/resource.h>
 #include <unistd.h>
-#include <linux/user.h>
+#include <sys/user.h>
+#include <sys/reg.h>
 #include <sys/syscall.h>   /* For SYS_write etc */
 #include <signal.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+
+pid_t child;
 
 void handler(int signo)
 {
@@ -14,101 +21,70 @@ void handler(int signo)
   kill(child, SIGKILL);
 }
 
+int isNumber(const char* arg)
+{
+  int i=0;
+  while(arg[i])
+  {
+    if(arg[i]<'0' || arg[i]>'9') return 0;
+    i++;
+  }
+  return 1;
+}
+
+int wait_for_syscall(pid_t child)
+{
+  int status;
+  while (1) {
+    ptrace(PTRACE_SYSCALL, child, 0, 0);
+    waitpid(child, &status, 0);
+    if(WIFEXITED(status)) return -1;
+    if(WIFSIGNALED(status)) return -1;
+    if(WIFSTOPPED(status) && (WSTOPSIG(status) & 0x80)) return 0;
+  }
+}
+
 int main(int argc, char** argv)
 {
-  //TODO
-  //Arg parsing
-    pid_t child;
-    long orig_eax, eax;
-    long params[3];
-    int status;
-    int insyscall = 0;
-    int executed = 0;
-    signal(SIGALRM, handler);
-    child = fork();
+  if(argc < 4 || !(isNumber(argv[1]) && isNumber(argv[2]))) exit(EXIT_FAILURE);
+  int MEM_LIMIT = atoi(argv[1]) * 1024 * 1024;
+  int TIMEOUT = atoi(argv[2]);
+  struct user_regs_struct regs;
+  int status;
+  int insyscall = 0;
+  int executed = 0;
+  int brked = 0;
+  signal(SIGALRM, handler);
+  child = fork();
+  if(child == 0)
+  {
+    struct rlimit mem_limit;
+    mem_limit.rlim_cur = mem_limit.rlim_max = MEM_LIMIT;
+    setrlimit(RLIMIT_AS, &mem_limit);
+    ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+    raise(SIGSTOP);
+    execvp(argv[3], &argv[3]);
+    exit(EXIT_FAILURE);
+  }
+  else
+  {
+    waitpid(child, &status, 0);
     alarm(TIMEOUT);
-    if(child == 0) {
-        struct rlimit mem_limit;
-        mem_limit.rlim_cur = mem_limit.rlim_max = MEM_LIMIT;
-        setrlimit(RLIMIT_AS, const struct rlimit *rlim);
-        ptrace(PTRACE_TRACEME, 0, NULL, NULL);
-        execl(argv[3], args);
+    ptrace(PTRACE_SETOPTIONS, child, 0, PTRACE_O_TRACESYSGOOD);
+    while(1)
+    {
+      if(wait_for_syscall(child)) break;
+      ptrace(PTRACE_GETREGS, child, 0, &regs);
+      if(!insyscall)
+      {
+        printf("SYSCALL %lld\n", regs.orig_rax);
+        insyscall = 1;
+      }
+      else
+      {
+        insyscall = 0;
+      }
     }
-    else {
-       while(1) {
-          wait(&status);
-          if(WIFEXITED(status))
-              break;
-          orig_eax = ptrace(PTRACE_PEEKUSER,
-                     child, 4 * ORIG_EAX, NULL);
-          if(orig_eax == SYS_write) {
-             if(insyscall == 0) {
-                /* Syscall entry */
-                insyscall = 1;
-                params[0] = ptrace(PTRACE_PEEKUSER,
-                                   child, 4 * EBX,
-                                   NULL);
-                if(params[0] != 1)
-                {
-                  printf("Forbidden access for write with first argument: %d\n", params[0]);
-                  kill(child, SIGKILL);
-                }
-                params[1] = ptrace(PTRACE_PEEKUSER,
-                                   child, 4 * ECX,
-                                   NULL);
-                params[2] = ptrace(PTRACE_PEEKUSER,
-                                   child, 4 * EDX,
-                                   NULL);
-                }
-             else { /* Syscall exit */
-
-                    insyscall = 0;
-                }
-            }
-            else if(orig_eax == SYS_read);
-            {
-              if(insyscall == 0) {
-                 /* Syscall entry */
-                 insyscall = 1;
-                 params[0] = ptrace(PTRACE_PEEKUSER,
-                                    child, 4 * EBX,
-                                    NULL);
-                 if(params[0] != 0)
-                 {
-                   printf("Forbidden access for read with first argument: %d\n", params[0]);
-                   kill(child, SIGKILL);
-                 }
-                 params[1] = ptrace(PTRACE_PEEKUSER,
-                                    child, 4 * ECX,
-                                    NULL);
-                 params[2] = ptrace(PTRACE_PEEKUSER,
-                                    child, 4 * EDX,
-                                    NULL);
-                 }
-               else { /* Syscall exit */
-
-                      insyscall = 0;
-                  }
-            }
-            else if(!executed && (orig_eax == SYS_execve))
-            {
-              if(insyscall == 0)
-              {
-                executed = 1;
-                insyscall = 1;
-              }
-              else{
-                insyscall = 0;
-              }
-            }
-            else
-            {
-              printf("Forbidden access for system call: %d\n", orig_eax);
-              kill(child, SIGKILL);
-            }
-            ptrace(PTRACE_SYSCALL,
-                   child, NULL, NULL);
-        }
-    }
-    return 0;
+  return 0;
+  }
 }
